a133f044f15f913395c49d34e25f97579fac73cb
Guillaume LASNIER <guillaume.lasnier@sap.com>
2024-02-29 17:46:55 +0100

    Cleanup

--- langchain_helloworld/technical-writer.py
+++ langchain_helloworld/technical-writer.py
@@ -7,7 +7,7 @@ import click
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import ConfigurableField, RunnableParallel, RunnablePassthrough
+from langchain_core.runnables import ConfigurableField
 from langchain_community.llms import Ollama
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.embeddings import OllamaEmbeddings
