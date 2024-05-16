292f925b873e78630466614a32bf743cbbed0de1
Guillaume LASNIER <guillaume.lasnier@sap.com>
2024-05-16 10:34:11 +0200

    Refactoring

--- langchain_helloworld/technical-writer.py
+++ langchain_helloworld/technical-writer.py
@@ -109,7 +109,7 @@ def setup_chain(model, conversational, retrieval):
         
         contextualize_q_prompt = ChatPromptTemplate.from_messages([
             SystemMessage('Given a chat history and the latest user question which might reference context in the chat history, '
-                + 'formulate a standalone question which can be understood without the chat history.'
+                + 'formulate a standalone question which can be understood without the chat history. '
                 + 'Do NOT answer the question, just reformulate it if needed and otherwise return it as is.'
             ),
             MessagesPlaceholder('chat_history'),
